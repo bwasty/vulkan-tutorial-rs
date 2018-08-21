54aa3403bfb2987e1efb3c0305898c4712994f2c
Benjamin Wasty <benny.wasty@gmail.com>
2018-08-22 01:04:51 +0200

    fix rendering & validation layer config

--- mac-env.sh
+++ mac-env.sh
@@ -3,3 +3,4 @@ export VULKAN_SDK=$HOME/Downloads/vulkansdk-macos-1.1.82.0/macOS
 export PATH=$VULKAN_SDK/bin:$PATH
 export DYLD_LIBRARY_PATH=$VULKAN_SDK/lib:$DYLD_LIBRARY_PATH
 export VK_ICD_FILENAMES=$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json
+export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
