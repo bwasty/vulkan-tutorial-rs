bdee7de4654fea912dfa25c62cb3d3e6ab035f92
Benjamin Wasty <benny.wasty@gmail.com>
2018-08-25 02:49:44 +0200

    physical device selection

---
+++ src/bin/03_physical_device_selection.rs
@@ -0,0 +1,194 @@
+extern crate vulkano;
+extern crate vulkano_win;
+extern crate winit;
+
+use std::sync::Arc;
+
+use winit::{ WindowBuilder, dpi::LogicalSize, Event, WindowEvent};
+use vulkano::instance::{
+    Instance,
+    InstanceExtensions,
+    ApplicationInfo,
+    Version,
+    layers_list,
+    PhysicalDevice,
+};
+use vulkano::instance::debug::{DebugCallback, MessageTypes};
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
+const VALIDATION_LAYERS: &[&str] =  &[
+    "VK_LAYER_LUNARG_standard_validation"
+];
+
+#[cfg(all(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = true;
+#[cfg(not(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = false;
+
+struct QueueFamilyIndices {
+    graphics_family: i32,
+    present_family: i32,
+}
+impl QueueFamilyIndices {
+    fn new() -> Self {
+        Self { graphics_family: -1, present_family: -1 }
+    }
+
+    fn is_complete(&self) -> bool {
+        self.graphics_family >= 0 && self.present_family >= 0
+    }
+}
+
+#[derive(Default)]
+struct HelloTriangleApplication {
+    instance: Option<Arc<Instance>>,
+    debug_callback: Option<DebugCallback>,
+    physical_device_index: usize, // can't store PhysicalDevice directly (lifetime issues)
+
+    events_loop: Option<winit::EventsLoop>,
+}
+
+impl HelloTriangleApplication {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn run(&mut self) {
+        self.init_window();
+        self.init_vulkan();
+        // self.main_loop();
+    }
+
+    fn init_window(&mut self) {
+        self.events_loop = Some(winit::EventsLoop::new());
+        // We'll leave this and the main loop commented out until we actually
+        // have something to show on screen.
+        let _window_builder = WindowBuilder::new()
+            .with_title("Vulkan")
+            .with_dimensions(LogicalSize::new(f64::from(WIDTH), f64::from(HEIGHT)));
+            // .build(&self.events_loop.as_ref().unwrap());
+    }
+
+    fn init_vulkan(&mut self) {
+        self.create_instance();
+        self.setup_debug_callback();
+        self.pick_physical_device();
+    }
+
+    fn create_instance(&mut self) {
+        if ENABLE_VALIDATION_LAYERS && !Self::check_validation_layer_support() {
+            println!("Validation layers requested, but not available!")
+        }
+
+        let supported_extensions = InstanceExtensions::supported_by_core()
+            .expect("failed to retrieve supported extensions");
+        println!("Supported extensions: {:?}", supported_extensions);
+
+        let app_info = ApplicationInfo {
+            application_name: Some("Hello Triangle".into()),
+            application_version: Some(Version { major: 1, minor: 0, patch: 0 }),
+            engine_name: Some("No Engine".into()),
+            engine_version: Some(Version { major: 1, minor: 0, patch: 0 }),
+        };
+
+        let required_extensions = Self::get_required_extensions();
+
+        let instance =
+            if ENABLE_VALIDATION_LAYERS && Self::check_validation_layer_support() {
+                Instance::new(Some(&app_info), &required_extensions, VALIDATION_LAYERS.iter().map(|s| *s))
+                    .expect("failed to create Vulkan instance")
+            } else {
+                Instance::new(Some(&app_info), &required_extensions, None)
+                    .expect("failed to create Vulkan instance")
+            };
+        self.instance = Some(instance);
+    }
+
+    fn check_validation_layer_support() -> bool {
+        let layers: Vec<_> = layers_list().unwrap().map(|l| l.name().to_owned()).collect();
+        VALIDATION_LAYERS.iter()
+            .all(|layer_name| layers.contains(&layer_name.to_string()))
+    }
+
+    fn get_required_extensions() -> InstanceExtensions {
+        let mut extensions = vulkano_win::required_extensions();
+        if ENABLE_VALIDATION_LAYERS {
+            // TODO!: this should be ext_debug_utils (_report is deprecated), but that doesn't exist yet in vulkano
+            extensions.ext_debug_report = true;
+        }
+
+        extensions
+    }
+
+    fn setup_debug_callback(&mut self) {
+        if !ENABLE_VALIDATION_LAYERS  {
+            return;
+        }
+
+        let instance = self.instance.as_ref().unwrap();
+        let msg_types = MessageTypes {
+            error: true,
+            warning: true,
+            performance_warning: true,
+            information: false,
+            debug: true,
+        };
+        self.debug_callback = DebugCallback::new(instance, msg_types, |msg| {
+            println!("validation layer: {:?}", msg.description);
+        }).ok();
+    }
+
+    fn pick_physical_device(&mut self) {
+        self.physical_device_index = PhysicalDevice::enumerate(&self.instance())
+            .position(|device| self.is_device_suitable(&device))
+            .expect("failed to find a suitable GPU!");
+    }
+
+    fn is_device_suitable(&self, device: &PhysicalDevice) -> bool {
+        let indices = self.find_queue_families(device);
+        indices.is_complete()
+    }
+
+    fn find_queue_families(&self, device: &PhysicalDevice) -> QueueFamilyIndices {
+        let mut indices = QueueFamilyIndices::new();
+        // TODO: replace index with id to simplify?
+        for (i, queue_family) in device.queue_families().enumerate() {
+            if queue_family.supports_graphics() {
+                indices.graphics_family = i as i32;
+            }
+
+            if indices.is_complete() {
+                break;
+            }
+        }
+
+        indices
+    }
+
+    #[allow(unused)]
+    fn main_loop(&mut self) {
+        loop {
+            let mut done = false;
+            self.events_loop.as_mut().unwrap().poll_events(|ev| {
+                match ev {
+                    Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => done = true,
+                    _ => ()
+                }
+            });
+            if done {
+                return;
+            }
+        }
+    }
+
+    fn instance(&self) -> &Arc<Instance> {
+        self.instance.as_ref().unwrap()
+    }
+}
+
+fn main() {
+    let mut app = HelloTriangleApplication::new();
+    app.run();
+}
